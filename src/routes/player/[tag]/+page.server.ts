3838ff92042c039e7934a185c5a3d28686e02a32
AlreadyHaveAnAcc <bwabersmanschool@gmail.com>
2023-03-16 00:44:35 -0700

    stricter ts

--- src/routes/player/[tag]/+page.server.ts
+++ src/routes/player/[tag]/+page.server.ts
@@ -1,8 +1,8 @@
 import { json, error } from '@sveltejs/kit';
 //@ts-ignore
 import { BRAWL_API_KEY } from '$env/static/private';
-// import BrawlStars from 'brawlstars-api-nodejs';
 import BrawlAPI from '$lib/utils/BrawlAPI';
+import { APIError } from '$lib/utils/Errors';
 
 interface Params {
 	tag: string;
@@ -15,32 +15,8 @@ export const load = async ({ params }: { params: Params }) => {
 		const playerData = client.getPlayer(params.tag);
 		return playerData;
 	} catch (err) {
+		if (!(err instanceof APIError)) throw error(500, JSON.stringify(err));
 		console.log(err.reason);
-		throw error(500, err.reason);
+		throw error(err.code, err.reason);
 	}
 };
-
-// let profile: any, battleLog: any
-// 	try {
-// 		[profile, battleLog] = await Promise.all([
-// 			(await fetch(`https://api.brawlstars.com/v1/players/${tag}`, {
-// 				method: 'GET',
-// 				headers: {
-// 					Authorization: `Bearer ${BRAWL_API_KEY}`
-// 				}
-// 			})).json(),
-// 			(await fetch(`https://api.brawlstars.com/v1/players/${tag}/battlelog`, {
-// 				method: 'GET',
-// 				headers: {
-// 					Authorization: `Bearer ${BRAWL_API_KEY}`
-// 				}
-// 			})).json()
-// 		]);
-// 	} catch (err) {
-//         throw error(404, new Error(err))
-//     }
-
-// 	return json({
-// 		profile: profile,
-//         battleLog: battleLog
-// 	});
