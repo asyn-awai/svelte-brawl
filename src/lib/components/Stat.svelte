4efca10db412abb6cd96b3d45d2c4e38811ff324
asyn-awai <95399051+asyn-awai@users.noreply.github.com>
2023-03-16 14:03:18 -0700

    2 files modified, 12 added, 11 deleted

---
+++ src/lib/components/Stat.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let name: string;
+    export let value: string;
+    import type { Mode } from '$lib/types'
+    // import icon from '$lib/images/img.png'
+
+    const getIcon = () => {
+        const icons: Partial<Record<Mode, string>> = {
+            
+        }   
+    }
+</script>
+
+<div class='relative flex items-center bg-[rgba(0, 0, 0, 0.4)] -skew-x-12 rounded-lg'>
+    <p class='absolute skew-x-12 -top-5 font-lilita font-bold text-outline-1 text-white text-center'>
+        {name}
+    </p>
+    <p class='skew-x-12'>
+        {stat}
+    </p>
+</div>
